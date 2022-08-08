d662563fe84dfafdba56378374fb2d9d8f1c6daa
Bárbara Broto <barbara.broto97@gmail.com>
2022-08-08 09:50:44 +0200

    Creada vista de opciones

--- MVC_1/src/main/java/Views/View.java
+++ MVC_1/src/main/java/Views/View.java
@@ -1,5 +1,67 @@
 package Views;
 
-public class View {
-	
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
+public class View extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					View frame = new View();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public View() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 437, 347);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Crear");
+		btnNewButton.setBounds(145, 123, 118, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Actualizar");
+		btnNewButton_1.setBounds(145, 161, 118, 23);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Consultar");
+		btnNewButton_2.setBounds(145, 195, 118, 23);
+		contentPane.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Eliminar");
+		btnNewButton_3.setBounds(145, 229, 118, 23);
+		contentPane.add(btnNewButton_3);
+		
+		JLabel lblNewLabel = new JLabel("Indica lo que deseas hacer");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel.setBounds(31, 47, 349, 32);
+		contentPane.add(lblNewLabel);
+	}
 }
