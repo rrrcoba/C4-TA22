30fe1eec1753210a7534e5a01c7accba272fdabc
rrrcoba <roger-decroly@hotmail.com>
2022-08-08 12:30:48 +0200

    controller and view changes

--- MVC_1/src/main/java/Controllers/Controller.java
+++ MVC_1/src/main/java/Controllers/Controller.java
@@ -2,6 +2,7 @@ package Controllers;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 
 import javax.swing.JFrame;
 
@@ -40,6 +41,12 @@ public class Controller implements ActionListener{
 		view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		view.setVisible(true);
 		view.setBounds(100, 100, 437, 347);
+		
+		// Ponemos las acciones a cada botón
+		view.getBtnCreate().addActionListener(this);
+		view.getBtnSelect().addActionListener(this);
+		view.getBtnUpdate().addActionListener(this);
+		view.getBtnDelete().addActionListener(this);
 	}
 
 	/**
@@ -47,8 +54,62 @@ public class Controller implements ActionListener{
 	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		if (view.getBtnCreate() == e.getSource()) {
+			//TODO acción del botón create
+		} else if (view.getBtnSelect() == e.getSource()) {
+			//TODO
+		} else if (view.getBtnUpdate() == e.getSource()) {
+			//TODO
+		} else if (view.getBtnDelete() == e.getSource()) {
+			//TODO
+		}
+		
+		System.out.println("Accion del botón OK");
+		
+	}
+	
+	/**
+	 * Transmite el nombre hacia la vista
+	 * 
+	 * @param name
+	 */
+	public void showName(String name) {
 		
 	}
 
+	/**
+	 * Transmite el apellido hacia la vista
+	 * 
+	 * @param surname
+	 */
+	public void showSurname(String surname) {
+		
+	}
+	
+	/**
+	 * Transmite la dirección hacia la vista
+	 * 
+	 * @param direction
+	 */
+	public void showDirection(String direction) {
+		
+	}
+	
+	/**
+	 * Transmite el dni hacia la vista
+	 * 
+	 * @param dni
+	 */
+	public void showDni(int dni) {
+		
+	}
+	
+	/**
+	 * Transmite la fecha hacia la vista
+	 * 
+	 * @param date
+	 */
+	public void showDate(Date date) {
+		
+	}
 }
--- MVC_1/src/main/java/Views/View.java
+++ MVC_1/src/main/java/Views/View.java
@@ -14,6 +14,14 @@ import javax.swing.SwingConstants;
 public class View extends JFrame {
 
 	private JPanel contentPane;
+	
+	/* Botones*/
+	private JButton btnCreate;
+	private JButton btnUpdate;
+	private JButton btnSelect;
+	private JButton btnDelete;
+
+	private JLabel lblMsg;
 
 	/**
 	 * Launch the application.
@@ -40,26 +48,96 @@ public class View extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnNewButton = new JButton("Crear");
-		btnNewButton.setBounds(145, 123, 118, 23);
-		contentPane.add(btnNewButton);
+		btnCreate = new JButton("Crear");
+		btnCreate.setBounds(145, 123, 118, 23);
+		contentPane.add(btnCreate);
 		
-		JButton btnNewButton_1 = new JButton("Actualizar");
-		btnNewButton_1.setBounds(145, 161, 118, 23);
-		contentPane.add(btnNewButton_1);
+		btnUpdate = new JButton("Actualizar");
+		btnUpdate.setBounds(145, 161, 118, 23);
+		contentPane.add(btnUpdate);
 		
-		JButton btnNewButton_2 = new JButton("Consultar");
-		btnNewButton_2.setBounds(145, 195, 118, 23);
-		contentPane.add(btnNewButton_2);
+		btnSelect = new JButton("Consultar");
+		btnSelect.setBounds(145, 195, 118, 23);
+		contentPane.add(btnSelect);
 		
-		JButton btnNewButton_3 = new JButton("Eliminar");
-		btnNewButton_3.setBounds(145, 229, 118, 23);
-		contentPane.add(btnNewButton_3);
+		btnDelete = new JButton("Eliminar");
+		btnDelete.setBounds(145, 229, 118, 23);
+		contentPane.add(btnDelete);
 		
-		JLabel lblNewLabel = new JLabel("Porfavor, indica lo que deseas hacer:");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblNewLabel.setBounds(31, 47, 349, 32);
-		contentPane.add(lblNewLabel);
+		lblMsg = new JLabel("Porfavor, indica lo que deseas hacer:");
+		lblMsg.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMsg.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblMsg.setBounds(31, 47, 349, 32);
+		contentPane.add(lblMsg);
+	}
+
+	/**
+	 * @return the btnCreate
+	 */
+	public JButton getBtnCreate() {
+		return btnCreate;
+	}
+
+	/**
+	 * @param btnCreate the btnCreate to set
+	 */
+	public void setBtnCreate(JButton btnCreate) {
+		this.btnCreate = btnCreate;
+	}
+
+	/**
+	 * @return the btnUpdate
+	 */
+	public JButton getBtnUpdate() {
+		return btnUpdate;
+	}
+
+	/**
+	 * @param btnUpdate the btnUpdate to set
+	 */
+	public void setBtnUpdate(JButton btnUpdate) {
+		this.btnUpdate = btnUpdate;
+	}
+
+	/**
+	 * @return the btnSelect
+	 */
+	public JButton getBtnSelect() {
+		return btnSelect;
+	}
+
+	/**
+	 * @param btnSelect the btnSelect to set
+	 */
+	public void setBtnSelect(JButton btnSelect) {
+		this.btnSelect = btnSelect;
+	}
+
+	/**
+	 * @return the btnDelete
+	 */
+	public JButton getBtnDelete() {
+		return btnDelete;
+	}
+
+	/**
+	 * @param btnDelete the btnDelete to set
+	 */
+	public void setBtnDelete(JButton btnDelete) {
+		this.btnDelete = btnDelete;
+	}
+
+	/**
+	 * @return the lblMsg
+	 */
+	public JLabel getLblMsg() {
+		return lblMsg;
+	}
+
+	/**
+	 * @param lblMsg the lblMsg to set
+	 */
+	public void setLblMsg(JLabel lblMsg) {
+		this.lblMsg = lblMsg;
 	}
 }
